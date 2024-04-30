a1d568f3e2da97998d25d23c3c0c0f71f5329871
Xinyu Mao <maoxinyu19990511@gmail.com>
2024-04-30 22:39:20 +1000

    Add files via upload

---
+++ chinese2english.py
@@ -0,0 +1,432 @@
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
+from typing import List
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import DataLoader, Dataset
+from timeit import default_timer as timer
+from torch.nn import Transformer
+from torch import Tensor
+from sklearn.model_selection import train_test_split
+from tqdm.auto import tqdm
+import torch.nn as nn
+import torch
+import jieba
+import numpy as np
+import math
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Set seed.
+seed = 42
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = True
+
+SRC_LANGUAGE = 'zh'
+TGT_LANGUAGE = 'en'
+
+EMB_SIZE = 192
+NHEAD = 8
+FFN_HID_DIM = 192
+BATCH_SIZE = 192
+NUM_ENCODER_LAYERS = 3
+NUM_DECODER_LAYERS = 3
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+NUM_EPOCHS = 10
+
+
+def jieba_tokenizer(text):
+    return list(jieba.cut(text))
+
+
+token_transform = {
+    SRC_LANGUAGE: jieba_tokenizer,
+    TGT_LANGUAGE: get_tokenizer('spacy', language='en_core_web_sm')
+}
+
+vocab_transform = {}
+
+csv = pd.read_json('translation2019zh/translation2019zh_train.json', lines=True)
+
+csv.head()
+
+train_csv, remaining_csv = train_test_split(csv, test_size=0.96)
+test_csv, _ = train_test_split(remaining_csv, test_size=0.995)
+
+
+# Custom Dataset class.
+class TranslationDataset(Dataset):
+    def __init__(self, data):
+        self.data = data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return (
+            self.data.iloc[idx]['chinese'],
+            self.data.iloc[idx]['english']
+        )
+
+
+train_dataset = TranslationDataset(train_csv)
+valid_dataset = TranslationDataset(test_csv)
+
+iterator = iter(train_dataset)
+print(next(iterator))
+
+
+# Helper function to yield list of tokens.
+def yield_tokens(data_iter, language):
+    language_column = {'zh': 'chinese', 'en': 'english'}[language]
+    for data_sample in data_iter[language_column]:
+        yield token_transform[language](data_sample)
+
+
+UNK_IDX, PAD_IDX, BOS_IDX, EOS_IDX = 0, 1, 2, 3
+# Make sure the tokens are in order of their indices to properly insert them in vocab.
+special_symbols = ["<bos>", "<eos>", "<pad>", "<unk>"]
+
+# Assuming train_csv is a DataFrame with 'chinese' and 'english' columns
+for ln in [SRC_LANGUAGE, TGT_LANGUAGE]:
+    column_name = {'zh': 'chinese', 'en': 'english'}
+    vocab_transform[ln] = build_vocab_from_iterator(
+        (token_transform[ln](text) for text in train_csv[column_name[ln]]),
+        min_freq=2,
+        specials=special_symbols,
+        special_first=True
+    )
+# Set ``UNK_IDX`` as the default index. This index is returned when the token is not found.
+# If not set, it throws ``RuntimeError`` when the queried token is not found in the Vocabulary.
+vocab_transform[SRC_LANGUAGE].set_default_index(UNK_IDX)
+vocab_transform[TGT_LANGUAGE].set_default_index(UNK_IDX)
+
+SRC_VOCAB_SIZE = len(vocab_transform[SRC_LANGUAGE])
+TGT_VOCAB_SIZE = len(vocab_transform[TGT_LANGUAGE])
+
+
+# helper function to club together sequential operations
+def sequential_transforms(*transforms):
+    def func(txt_input):
+        for transform in transforms:
+            txt_input = transform(txt_input)
+        return txt_input
+
+    return func
+
+
+# function to add BOS/EOS and create tensor for input sequence indices
+def tensor_transform(token_ids: List[int]):
+    return torch.cat((torch.tensor([BOS_IDX]),
+                      torch.tensor(token_ids),
+                      torch.tensor([EOS_IDX])))
+
+
+# `src` and `tgt` language text transforms to convert raw strings into tensor indices
+text_transform = {}
+for ln in [SRC_LANGUAGE, TGT_LANGUAGE]:
+    text_transform[ln] = sequential_transforms(token_transform[ln],  # Tokenization
+                                               vocab_transform[ln],  # Numericalization
+                                               tensor_transform)  # Add BOS/EOS and create tensor
+
+
+# function to collate data samples into batch tensors
+def collate_fn(batch):
+    src_batch, tgt_batch = [], []
+    for src_sample, tgt_sample in batch:
+        src_batch.append(text_transform[SRC_LANGUAGE](src_sample))
+        tgt_batch.append(text_transform[TGT_LANGUAGE](tgt_sample))
+    src_batch = pad_sequence(src_batch, padding_value=PAD_IDX, batch_first=True)
+    tgt_batch = pad_sequence(tgt_batch, padding_value=PAD_IDX, batch_first=True)
+    return src_batch, tgt_batch
+
+
+def generate_square_subsequent_mask(sz):
+    mask = (torch.triu(torch.ones((sz, sz), device=DEVICE)) == 1).transpose(0, 1)
+    mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+    return mask
+
+
+def create_mask(src, tgt):
+    src_seq_len = src.shape[1]
+    tgt_seq_len = tgt.shape[1]
+    tgt_mask = generate_square_subsequent_mask(tgt_seq_len)
+    src_mask = torch.zeros((src_seq_len, src_seq_len), device=DEVICE).type(torch.float)
+    src_padding_mask = (src == PAD_IDX).type(torch.float)
+    tgt_padding_mask = (tgt == PAD_IDX).type(torch.float)
+    return src_mask, tgt_mask, src_padding_mask, tgt_padding_mask
+
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout, max_len=5000):
+        """
+        :param max_len: Input length sequence.
+        :param d_model: Embedding dimension.
+        :param dropout: Dropout value (default=0.1)
+        """
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        """
+        Inputs of forward function
+        :param x: the sequence fed to the positional encoder model (required).
+        Shape:
+            x: [sequence length, batch size, embed dim]
+            output: [sequence length, batch size, embed dim]
+        """
+        x = x + self.pe[:, :x.size(1)]
+        return self.dropout(x)
+
+
+class TokenEmbedding(nn.Module):
+    def __init__(self, vocab_size: int, emb_size):
+        super(TokenEmbedding, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, emb_size)
+        self.emb_size = emb_size
+
+    def forward(self, tokens: Tensor):
+        return self.embedding(tokens.long()) * math.sqrt(self.emb_size)
+
+
+class Seq2SeqTransformer(nn.Module):
+    def __init__(
+            self,
+            num_encoder_layers: int,
+            num_decoder_layers: int,
+            emb_size: int,
+            nhead: int,
+            src_vocab_size: int,
+            tgt_vocab_size: int,
+            dim_feedforward: int = 512,
+            dropout: float = 0.1
+    ):
+        super(Seq2SeqTransformer, self).__init__()
+        self.transformer = Transformer(
+            d_model=emb_size,
+            nhead=nhead,
+            num_encoder_layers=num_encoder_layers,
+            num_decoder_layers=num_decoder_layers,
+            dim_feedforward=dim_feedforward,
+            dropout=dropout,
+            batch_first=True
+        )
+        self.generator = nn.Linear(emb_size, tgt_vocab_size)
+        self.src_tok_emb = TokenEmbedding(src_vocab_size, emb_size)
+        self.tgt_tok_emb = TokenEmbedding(tgt_vocab_size, emb_size)
+        self.positional_encoding = PositionalEncoding(
+            emb_size, dropout=dropout)
+
+    def forward(self,
+                src: Tensor,
+                trg: Tensor,
+                src_mask: Tensor,
+                tgt_mask: Tensor,
+                src_padding_mask: Tensor,
+                tgt_padding_mask: Tensor,
+                memory_key_padding_mask: Tensor):
+        src_emb = self.positional_encoding(self.src_tok_emb(src))
+        tgt_emb = self.positional_encoding(self.tgt_tok_emb(trg))
+        outs = self.transformer(src_emb, tgt_emb, src_mask, tgt_mask, None,
+                                src_padding_mask, tgt_padding_mask, memory_key_padding_mask)
+        return self.generator(outs)
+
+    def encode(self, src: Tensor, src_mask: Tensor):
+        return self.transformer.encoder(self.positional_encoding(
+            self.src_tok_emb(src)), src_mask)
+
+    def decode(self, tgt: Tensor, memory: Tensor, tgt_mask: Tensor):
+        return self.transformer.decoder(self.positional_encoding(
+            self.tgt_tok_emb(tgt)), memory,
+            tgt_mask)
+
+
+model = Seq2SeqTransformer(
+    NUM_ENCODER_LAYERS,
+    NUM_DECODER_LAYERS,
+    EMB_SIZE,
+    NHEAD,
+    SRC_VOCAB_SIZE,
+    TGT_VOCAB_SIZE,
+    FFN_HID_DIM
+).to(DEVICE)
+# Total parameters and trainable parameters.
+total_params = sum(p.numel() for p in model.parameters())
+print(f"{total_params:,} total parameters.")
+total_trainable_params = sum(
+    p.numel() for p in model.parameters() if p.requires_grad)
+print(f"{total_trainable_params:,} training parameters.")
+print(model)
+
+loss_fn = torch.nn.CrossEntropyLoss(ignore_index=PAD_IDX)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.98), eps=1e-9)
+
+train_dataloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, collate_fn=collate_fn)
+
+
+def train_epoch(model, optimizer):
+    print('Training')
+    model.train()
+    losses = 0
+    for src, tgt in tqdm(train_dataloader, total=len(list(train_dataloader))):
+        # print(" ".join(vocab_transform[SRC_LANGUAGE].lookup_tokens(list(src[0].cpu().numpy()))).replace("<bos>", "").replace("<eos>", ""))
+        # print(" ".join(vocab_transform[TGT_LANGUAGE].lookup_tokens(list(tgt[0].cpu().numpy()))).replace("<bos>", "").replace("<eos>", ""))
+        src = src.to(DEVICE)
+        tgt = tgt.to(DEVICE)
+
+        tgt_input = tgt[:, :-1]
+
+        src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(src, tgt_input)
+        logits = model(
+            src,
+            tgt_input,
+            src_mask,
+            tgt_mask,
+            src_padding_mask,
+            tgt_padding_mask,
+            src_padding_mask
+        )
+        optimizer.zero_grad()
+        tgt_out = tgt[:, 1:]
+        loss = loss_fn(logits.view(-1, TGT_VOCAB_SIZE), tgt_out.contiguous().view(-1))
+        loss.backward()
+        optimizer.step()
+        losses += loss.item()
+    return losses / len(list(train_dataloader))
+
+
+val_dataloader = DataLoader(valid_dataset, batch_size=BATCH_SIZE, collate_fn=collate_fn)
+
+
+def evaluate(model):
+    print('Validating')
+    model.eval()
+    losses = 0
+    for src, tgt in tqdm(val_dataloader, total=len(list(val_dataloader))):
+        # print(" ".join(vocab_transform[SRC_LANGUAGE].lookup_tokens(list(src[0].cpu().numpy()))).replace("<bos>", "").replace("<eos>", ""))
+        # print(" ".join(vocab_transform[TGT_LANGUAGE].lookup_tokens(list(tgt[0].cpu().numpy()))).replace("<bos>", "").replace("<eos>", ""))
+        src = src.to(DEVICE)
+        tgt = tgt.to(DEVICE)
+
+        tgt_input = tgt[:, :-1]
+
+        src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(src, tgt_input)
+
+        logits = model(
+            src,
+            tgt_input,
+            src_mask,
+            tgt_mask,
+            src_padding_mask,
+            tgt_padding_mask,
+            src_padding_mask
+        )
+        tgt_out = tgt[:, 1:]
+        loss = loss_fn(logits.view(-1, TGT_VOCAB_SIZE), tgt_out.contiguous().view(-1))
+        losses += loss.item()
+    return losses / len(list(val_dataloader))
+
+
+train_loss_list, valid_loss_list = [], []
+for epoch in range(1, NUM_EPOCHS + 1):
+    start_time = timer()
+    train_loss = train_epoch(model, optimizer)
+    valid_loss = evaluate(model)
+    end_time = timer()
+    train_loss_list.append(train_loss)
+    valid_loss_list.append(valid_loss)
+    print((
+        f"Epoch: {epoch}, Train loss: {train_loss:.3f}, Val loss: {valid_loss:.3f}, "f"Epoch time = {(end_time - start_time):.3f}s \n"))
+
+os.makedirs('outputs', exist_ok=True)
+
+
+def save_plots(train_loss, valid_loss):
+    """
+    Function to save the loss plots to disk.
+    """
+    # Loss plots.
+    plt.figure(figsize=(10, 7))
+    plt.plot(
+        train_loss, color='blue', linestyle='-',
+        label='train loss'
+    )
+    plt.plot(
+        valid_loss, color='red', linestyle='-',
+        label='validataion loss'
+    )
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.savefig(os.path.join('outputs', 'loss.png'))
+    plt.show()
+
+
+save_plots(train_loss_list, valid_loss_list)
+torch.save(model, 'outputs/model.pth')
+torch.save(src_vocab, 'outputs/src_vocab.pth')
+torch.save(tgt_vocab, 'outputs/tgt_vocab.pth')
+
+model = torch.load('outputs/model.pth')
+
+
+# Helper function to generate output sequence using greedy algorithm.
+def greedy_decode(model, src, src_mask, max_len, start_symbol):
+    src = src.to(DEVICE)
+    src_mask = src_mask.to(DEVICE)
+    memory = model.encode(src, src_mask)
+    ys = torch.ones(1, 1).fill_(start_symbol).type(torch.long).to(DEVICE)
+    for i in range(max_len - 1):
+        memory = memory.to(DEVICE)
+        if i == 0:
+            ys = ys.transpose(1, 0)
+        tgt_mask = (generate_square_subsequent_mask(ys.size(1))
+                    .type(torch.bool)).to(DEVICE)
+        out = model.decode(ys, memory, tgt_mask)
+        out = out
+        prob = model.generator(out[:, -1])
+        _, next_word = torch.max(prob, dim=1)
+        next_word = next_word.item()
+        ys = torch.cat([ys,
+                        torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=1)
+        if next_word == EOS_IDX:
+            break
+    return ys
+
+
+# Translation function.
+def translate(model: torch.nn.Module, src_sentence: str):
+    model.eval()
+    src = text_transform[SRC_LANGUAGE](src_sentence).view(1, -1)
+    num_tokens = src.shape[1]
+    src_mask = (torch.zeros(num_tokens, num_tokens)).type(torch.bool)
+    tgt_tokens = greedy_decode(
+        model, src, src_mask, max_len=num_tokens + 5, start_symbol=BOS_IDX).flatten()
+    return " ".join(vocab_transform[TGT_LANGUAGE].lookup_tokens(list(tgt_tokens.cpu().numpy()))).replace("<bos>",
+                                                                                                         "").replace(
+        "<eos>", "")
+
+
+# SRC, GT pairs from the validation set.
+infer_sentences = [
+    ["拿走他的收入，毫无疑问就是盗窃。", "So to take it from him is by definition theft."],
+    ["时间是一个主轴，所以我必须解决其中心。", "Time is a spindle so I must resolve its center."],
+    ["那座城镇在一群游击队员控制之下。", "The town lies within the control of a group of guerrillas."],
+    ["但是在太空上面应该怎么办呢?", "But how does that work in space?"],
+    ["镇痛剂由于本身的成瘾性及毒副反应，在一定程度上限制了使用", "Application of analgesics is limited by its addiction and side effects."]
+]
+for sentence in infer_sentences:
+    print(f"SRC: {sentence[0]}")
+    print(f"GT: {sentence[1]}")
+    print(f"PRED: {translate(model, sentence[0])}\n")
